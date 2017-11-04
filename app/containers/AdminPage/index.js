b4f4cc05e4001f43bdeebc92cc1de6457cba769f
Steven Goff <steveng9@gmail.com>
2017-11-04 13:13:22 -0500

    Generate AdminPage

---
+++ app/containers/AdminPage/index.js
@@ -0,0 +1,51 @@
+/**
+ *
+ * AdminPage
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { compose } from 'redux';
+
+import injectSaga from 'utils/injectSaga';
+import injectReducer from 'utils/injectReducer';
+import makeSelectAdminPage from './selectors';
+import reducer from './reducer';
+import saga from './saga';
+
+export class AdminPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
+  render() {
+    return (
+      <div>
+      </div>
+    );
+  }
+}
+
+AdminPage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = createStructuredSelector({
+  adminpage: makeSelectAdminPage(),
+});
+
+function mapDispatchToProps(dispatch) {
+  return {
+    dispatch,
+  };
+}
+
+const withConnect = connect(mapStateToProps, mapDispatchToProps);
+
+const withReducer = injectReducer({ key: 'adminPage', reducer });
+const withSaga = injectSaga({ key: 'adminPage', saga });
+
+export default compose(
+  withReducer,
+  withSaga,
+  withConnect,
+)(AdminPage);
